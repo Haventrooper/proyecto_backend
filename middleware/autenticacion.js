cb7ebf345d5582bac6051d7ae4c0fc9651adecae
Diego R <diego.riveros.g@mail.pucv.cl>
2023-10-27 13:27:40 -0300

    Prueba middleware y consultas

---
+++ middleware/autenticacion.js
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken';
+
+function auth(req, res, next) {
+    try {
+        let token = req.headers.authorization.split(" ")[1]
+        let id_usuario = jwt.verify(token, 'cualquiercosa')
+        req.id_usuario = id_usuario.usuario
+        console.log(req.id_usuario)
+        next()
+    }
+    catch(e){
+        console.error(e)
+        res.status(401).send()
+    }
+}
+export default auth
--- routes/router.js
+++ routes/router.js
@@ -2,6 +2,7 @@
 
 import postgres from 'postgres';
 import jwt from 'jsonwebtoken';
+import auth from '../middleware/autenticacion.js';
 
 const sql = postgres({
     host: '127.0.0.1',
@@ -12,17 +13,52 @@ const sql = postgres({
 
 function routes(app){
     app.get('/login', async (req, res) => {
-        let contrasena = await sql`
+        let response = await sql`
         select * from usuarios 
         where nombre = ${req.query.nombre} and contrasena = ${req.query.contrasena}
         `
-        if (contrasena.length == 0){
+        if (response.length == 0){
             res.send(false)
         }else{
-            let token = jwt.sign({"usuario": req.query.nombre},'cualquiercosa')
+            let token = jwt.sign({"usuario": response[0].id_usuario},'cualquiercosa')
             res.send(token)
-        }    
-    })    
+        }
+    })
+
+    app.get('/leerTema', auth, async (req, res) => {
+        
+        try {
+            let tema = await sql`
+            select temas.tema
+            from usuarios join temas
+            on usuarios.id_tema = temas.id_tema
+            where usuarios.id_usuario = ${req.id_usuario}
+        `
+            res.json(tema[0]) //primer tema
+
+        }
+        catch(e){
+            console.log(e)
+            res.status(500).send()
+        }
+        
+    })
+
+    app.get('/perrosUsuario', auth, async (req, res) => {
+        try {
+            let perros = await sql`
+            select * from perros
+            where perros.id_usuario = ${req.id_usuario}
+            `
+
+            res.json(perros) //array completo
+        }
+        catch(e){
+            console.log(e)
+            res.status(500).send()
+        }
+    })
+
 }
 export default routes
 
